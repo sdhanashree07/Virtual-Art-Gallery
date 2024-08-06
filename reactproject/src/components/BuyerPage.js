4081c836954c3b31eb9406f485f870b3ef9ca26b
swapnil <swapnilbiradar20@gmail.com>
2024-08-06 12:16:35 +0530

    React All project Added

---
+++ reactproject/src/components/BuyerPage.js
@@ -0,0 +1,10 @@
+// components/BuyerPage.js
+export default function BuyerPage() {
+    return (
+      <div className="container">
+        <h1>Welcome, Buyer!</h1>
+        <p>Here you can browse and purchase artworks.</p>
+      </div>
+    );
+  }
+  
