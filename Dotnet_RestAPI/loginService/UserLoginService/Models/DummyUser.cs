818a8afd2328b421367c021c9bbbd0dd9545535e
moin rogatiya <mmoin1722@gmail.com>
2024-08-26 11:09:19 +0530

    Dotnet RestAPI

---
+++ Dotnet_RestAPI/loginService/UserLoginService/Models/DummyUser.cs
@@ -0,0 +1,10 @@
+﻿namespace UserLoginService.Models
+{
+    public class DummyUser
+    {
+
+        public string Username { get; set; } = null!;
+        public string Password { get; set; } = null!;
+
+    }
+}
